e7852f3ce396e899895ed86803adb05ef66ada2b
iamjonguz <iamjonguz@gmail.com>
2021-05-31 20:57:37 +0200

    added auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
 
 function App() {
   return (
     <div className="App">
       PLACEHOLDER 
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
